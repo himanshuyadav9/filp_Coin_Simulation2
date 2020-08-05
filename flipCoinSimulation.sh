a19aeeb1220ceff4350c5a3db5939832d5c886b5
Himanshu <yadavhimanshu63@gmail.com>
2020-08-05 22:17:47 +0530

    doublet

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -3,7 +3,6 @@
 
 echo "Welcome to flip coin simulation2"
 
-
 result=$((RANDOM%2+1))
 
 COUNT_HEAD=0
@@ -13,28 +12,49 @@ TEMP=$userInput
 
 declare -A FlipingCoin
 
+COUNT_HH=0		#HH=Head Head
+COUNT_HT=0		#HT=Head Tail
+COUNT_TT=0		#TT=Tail Tail
+COUNT_TH=0		#TH=Tail Tail
+
+declare -A FlipingCoin
+
 for(( i=1; i<=$userInput; i++ ))
 	do
 	
-		result=$((RANDOM%2+1))
+	result=$((RANDOM%4+1))
+	
 		case $result in
 			1)
-				COUNT_HEAD=$(($COUNT_HEAD+1))
-				FlipingCoin["H"]="$COUNT_HEAD"
+				COUNT_HH=$(($COUNT_HH+1))
+				FlipingCoin["HH"]="$COUNT_HH"
 				;;
 			2)
-				COUNT_TAIL=$(($COUNT_TAIL+1))
-				FlipingCoin["T"]="$COUNT_TAIL"
+				COUNT_HT=$(($COUNT_HT+1))
+				FlipingCoin["HT"]="$COUNT_HT"
 				;;
-		esac
+			3)
+				COUNT_TT=$(($COUNT_TT+1))
+				FlipingCoin["TT"]="$COUNT_TT"
+				;;
+			4)
+				COUNT_TH=$(($COUNT_TH+1))
+				FlipingCoin["TH"]="$COUNT_TH"
+				;;
+			esac
 	done
-headPercentage=`awk "BEGIN{print ($COUNT_HEAD/$TEMP)*100}"`
-tailPercentage=`awk "BEGIN{print ($COUNT_TAIL/$TEMP)*100}"`
+
+headHeadPercentage=`awk "BEGIN{print ($COUNT_HH/$TEMP)*100}"`
+headTailPercentage=`awk "BEGIN{print ($COUNT_HT/$TEMP)*100}"`
+tailTailPercentage=`awk "BEGIN{print ($COUNT_TT/$TEMP)*100}"`
+tailHeadPercentage=`awk "BEGIN{print ($COUNT_TH/$TEMP)*100}"`
 
 for count in ${!FlipingCoin[@]}
-	do
-		echo "$count : ${FlipingCoin[$count]}"
-	done
+do
+	echo "$count : ${FlipingCoin[$count]}"
+done
 
-echo "Head percentage : $headPercentage "
-echo "Tail percentage : $tailPercentage "
+echo "Head head percentage : $headHeadPercentage"
+echo "Head tail percentage : $headTailPercentage"
+echo "Tail tail percentage : $tailTailPercentage"
+echo "Tail head percentage : $tailHeadPercentage"
