23071bfcbf68b6a7262b4a3210acb0faa1074503
Himanshu <yadavhimanshu63@gmail.com>
2020-08-05 23:21:18 +0530

    singleDoubleTriplet

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -89,3 +89,129 @@ echo "Tail tail tail percentage : $tttPercentage"
 echo "Tail tail head percentage : $tthPercentage"
 echo "Tail head tail percentage : $thtPercentage"
 echo "Head Tail tail percentage : $httPercentage"
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function doublet
+{
+
+result=$((RANDOM%2+1))
+
+COUNT_HEAD=0
+COUNT_TAIL=0
+read -p "Enter how many time to flip coin: " userInput
+TEMP=$userInput
+
+declare -A FlipingCoin
+
+COUNT_HH=0		#HH=Head Head
+COUNT_HT=0		#HT=Head Tail
+COUNT_TT=0		#TT=Tail Tail
+COUNT_TH=0		#TH=Tail Tail
+
+for(( i=1; i<=$userInput; i++ ))
+	do
+	
+	result=$((RANDOM%4+1))
+	
+		case $result in
+			1)
+				COUNT_HH=$(($COUNT_HH+1))
+				FlipingCoin["HH"]="$COUNT_HH"
+				;;
+			2)
+				COUNT_HT=$(($COUNT_HT+1))
+				FlipingCoin["HT"]="$COUNT_HT"
+				;;
+			3)
+				COUNT_TT=$(($COUNT_TT+1))
+				FlipingCoin["TT"]="$COUNT_TT"
+				;;
+			4)
+				COUNT_TH=$(($COUNT_TH+1))
+				FlipingCoin["TH"]="$COUNT_TH"
+				;;
+			esac
+	done
+
+headHeadPercentage=`awk "BEGIN{print ($COUNT_HH/$TEMP)*100}"`
+headTailPercentage=`awk "BEGIN{print ($COUNT_HT/$TEMP)*100}"`
+tailTailPercentage=`awk "BEGIN{print ($COUNT_TT/$TEMP)*100}"`
+tailHeadPercentage=`awk "BEGIN{print ($COUNT_TH/$TEMP)*100}"`
+
+for sort in ${!FlipingCoin[@]}
+do
+	echo "$sort : ${FlipingCoin[$sort]}"
+done |
+sort -rn -k3
+
+echo "Head head percentage : $headHeadPercentage"
+echo "Head tail percentage : $headTailPercentage"
+echo "Tail tail percentage : $tailTailPercentage"
+echo "Tail head percentage : $tailHeadPercentage"
+}
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+function singlet
+{
+
+result=$((RANDOM%2+1))
+
+COUNT_HEAD=0
+COUNT_TAIL=0
+read -p "Enter how many time to flip coin: " userInput
+TEMP=$userInput
+
+for(( i=1; i<=$userInput; i++ ))
+	do
+		
+		result=$((RANDOM%2+1))
+		case $result in
+			1)
+				COUNT_HEAD=$(($COUNT_HEAD+1))
+				FlipingCoin["H"]="$COUNT_HEAD"
+				;;
+			2)
+				COUNT_TAIL=$(($COUNT_TAIL+1))
+				FlipingCoin["T"]="$COUNT_TAIL"
+				;;
+		esac
+	done
+headPercentage=`awk "BEGIN{print ($COUNT_HEAD/$TEMP)*100}"`
+tailPercentage=`awk "BEGIN{print ($COUNT_TAIL/$TEMP)*100}"`
+
+for sort in ${!FlipingCoin[@]}
+do
+	echo "$sort : ${FlipingCoin[$sort]}"
+done |
+sort -rn -k3
+
+echo "Head percentage : $headPercentage "
+echo "Tail percentage : $tailPercentage "
+}
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+echo "Welcome to flip coin simulation"
+echo "Enter 1 for singlet"
+echo "Enter 2 for dublet"
+echo "Enter 3 for triplet"
+read number
+case $number in
+	1)
+		singlet
+		;;
+
+	2)
+		doublet
+		;;
+
+	3)
+		triplet
+		;;
+
+	*)
+		echo "Invalid input"
+esac
